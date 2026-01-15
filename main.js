478394508f4d56920fde3ee74277104f8b6614bd
DraconicalX <tonchen@nuevaschool.org>
2026-01-15 14:04:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function setup(){
+  let cnv = createCanvas(windowWidth, windowHeight);
+  
+  cnv.parent(document.querySelector('main'));
+  
+  background(220);
+}
 function draw(){
   text("Test!",width/2,height/2)
 }
