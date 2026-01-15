0e1b041bd9d784b2acecfcb3be59db9f66581d31
DraconicalX <tonchen@nuevaschool.org>
2026-01-15 14:20:06 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,37 @@ function setup(){
   cnv.parent(document.querySelector('main'));
   
   background(220);
+  var status = 0;
 }
 function draw(){
-  text("Test!",width/2,height/2)
+  text("Enter website password:",width/2,height/2+height/16)
+  if(keyIsDown(LEFT_ARROW)&&status = 0;){
+    status++;
+  }else if(keyIsDown(UP_ARROW)&&status = 1;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 2;){
+    status++;
+  }else if(keyIsDown(DOWN_ARROW)&&status = 3;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 4;){
+    status++;
+  }else if(keyIsDown(DOWN_ARROW)&&status = 5;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 6;){
+    status++;
+  }if(keyIsDown(LEFT_ARROW)&&status = 7;){
+    status++;
+  }else if(keyIsDown(UP_ARROW)&&status = 8;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 9;){
+    status++;
+  }else if(keyIsDown(DOWN_ARROW)&&status = 10;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 11;){
+    status++;
+  }else if(keyIsDown(DOWN_ARROW)&&status = 12;){
+    status++;
+  }else if(keyIsDown(RIGHT_ARROW)&&status = 13;){
+    status++;
+  }
 }
