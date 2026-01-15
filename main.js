d18e5f342a906b0fd19ab7e96306b975a9be517c
DraconicalX <tonchen@nuevaschool.org>
2026-01-15 14:28:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var status
+var lock
 function setup(){
   let cnv = createCanvas(windowWidth, windowHeight);
   
@@ -6,36 +7,41 @@ function setup(){
   
   background(220);
   status = 0;
+  lock = true
 }
 function draw(){
-  text("Enter website password:",width/2,height/2+height/16);
-  if(keyIsDown(LEFT_ARROW)&&status == 0){
-    status++;
-  }else if(keyIsDown(UP_ARROW)&&status == 1){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 2){
-    status++;
-  }else if(keyIsDown(DOWN_ARROW)&&status == 3){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 4){
-    status++;
-  }else if(keyIsDown(DOWN_ARROW)&&status == 5){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 6){
-    status++;
-  }if(keyIsDown(LEFT_ARROW)&&status == 7){
-    status++;
-  }else if(keyIsDown(UP_ARROW)&&status == 8){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 9){
-    status++;
-  }else if(keyIsDown(DOWN_ARROW)&&status == 10){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 11){
-    status++;
-  }else if(keyIsDown(DOWN_ARROW)&&status == 12){
-    status++;
-  }else if(keyIsDown(RIGHT_ARROW)&&status == 13){
-    status++;
+  if(lock){
+    text("Enter website password:",width/2,height/2+height/16);
+    if(keyIsDown(LEFT_ARROW)&&status == 0){
+      status++;
+    }else if(keyIsDown(UP_ARROW)&&status == 1){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 2){
+      status++;
+    }else if(keyIsDown(DOWN_ARROW)&&status == 3){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 4){
+      status++;
+    }else if(keyIsDown(DOWN_ARROW)&&status == 5){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 6){
+      status++;
+    }if(keyIsDown(LEFT_ARROW)&&status == 7){
+      status++;
+    }else if(keyIsDown(UP_ARROW)&&status == 8){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 9){
+      status++;
+    }else if(keyIsDown(DOWN_ARROW)&&status == 10){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 11){
+      status++;
+    }else if(keyIsDown(DOWN_ARROW)&&status == 12){
+      status++;
+    }else if(keyIsDown(RIGHT_ARROW)&&status == 13){
+      status++;
+    }else if(keyIsDown(UP_ARROW)&&status == 14){
+      lock = false;
+    }
   }
 }
