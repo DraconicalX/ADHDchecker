ca5b5b89584f47b1c378f665d5a40eff05283adc
DraconicalX <tonchen@nuevaschool.org>
2026-02-10 14:46:07 -0800

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function draw(){
     textAlign(CENTER, CENTER);
     fill('white')
     text("Tester tester Tester! ABCDEFG",200,200)
-    image(button1,width/8,height/8,0,7*height/8)
-    image(button2,width/8,height/8,width/8+width/16,7*height/8)
+    image(button1,width*0.1/8,height*0.1/8,0,7*height*0.1/8)
+    image(button2,width*0.1/8,height*0.1/8,width*0.1/8+width*0.1/16,7*height*0.1/8)
   }
 }
